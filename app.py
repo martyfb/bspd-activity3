d2670b8c216882fc5b8a3ff5b7935171978ff817
Faith Marty <faithmarty05@gmai.com>
2025-02-10 15:36:51 +0800

    php-app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to flask home"
+
+
+@app.route('/page1')
+def page1():
+    return "Welcome to flask flask page1"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
