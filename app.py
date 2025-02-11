4704b80a5c42fe369cf874b3775af90b9f648c06
Faith Marty <faithmarty05@gmai.com>
2025-02-11 13:40:55 +0800

    act6

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/page1')
 def page1():
-    return "Welcome to flask flask page1"
+    return "Welcome to flask page1"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
